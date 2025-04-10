070d661de602527a39da41928395ce4c97f7cb82
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-04-10 21:01:54 +0530

    Create object-1.js

---
+++ Objects/object-1.js
@@ -0,0 +1,15 @@
+const product = {
+  name: 'pen',
+  price: 10
+};
+
+console.log(product);
+console.log(product.name);
+console.log(product.price);
+
+product.newProduct = 'Pen-pencil'
+console.log(product);
+
+delete product.newProduct
+
+console.log(product);
