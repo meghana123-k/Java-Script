71c2aa25efeb689f799781d9920ecc6c8c67fd67
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-04-12 11:03:13 +0530

    Create object-2.js
    Created product object and updated price using . and added delivery time into product object using []

---
+++ Objects/object-2.js
@@ -0,0 +1,12 @@
+const product = {
+  name: "Basket Ball",
+  price: 2095
+}
+
+console.log(product);
+
+let updatedPrice = product.price + 50;
+product.price = updatedPrice
+
+product['delivery-time'] = "3 days"
+console.log(product);
