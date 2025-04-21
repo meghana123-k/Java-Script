0cb961ca429c2f867e63c0b1ed35e17bcf98c7a7
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-04-21 14:28:09 +0530

    Add files via upload

---
+++ Advanced Functions/todolist/script.js
@@ -0,0 +1,40 @@
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "22-06-2025",
+  },
+  {
+    name: "wash utensils",
+    dueDate: "24-06-2025",
+  },
+];
+renderTodoList();
+function renderTodoList() {
+  let todoListHTML = "";
+  todoList.forEach(function(todoObject, index){
+    const { name, dueDate } = todoObject;
+    const html = `
+          <div>${name}</div>
+          <div>${dueDate}</div>
+            <button onclick = "
+            todoList.splice(${index}, 1)
+            renderTodoList();
+            " class = "delete-todo-button">Delete</button>
+      
+          `;
+    todoListHTML += html;
+  });
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+function addItems() {
+  const inputElement = document.querySelector(".js-todo-name");
+  const inputDueDate = document.querySelector(".js-todo-dueDate");
+
+  const name = inputElement.value;
+  const dueDate = inputDueDate.value;
+  todoList.push({ name, dueDate });
+  inputElement.value = "";
+  inputDueDate.value = "";
+  renderTodoList();
+}
