a0899bf9e38306326f48e38db1baa1b2837faeda
meghana123-k <kothakondameghana3@gmail.com>
2026-02-13 13:33:33 +0530

    revision on loops

--- Revision/arrays.js
+++ Revision/arrays.js
@@ -1,5 +1,11 @@
 const a = [1, 2, 3, 4, 5]
 
-for(let num : a) {
+for(let i = 0; i < a.length; i++) {
+	console.log(a[i]);
+}
+for(let num of a) {
+	console.log(num);
+}
+for(let num in a) {
 	console.log(num);
 }
