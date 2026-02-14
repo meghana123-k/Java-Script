98b3f80916142c8b66244c13d0b130cda35a0098
meghana123-k <kothakondameghana3@gmail.com>
2026-02-14 17:37:34 +0530

    git commit -m "added map, filter and reduce examples"

--- Revision/mapReduceFilter.js
+++ Revision/mapReduceFilter.js
@@ -1,5 +1,5 @@
 // map, filter, reduce
-let nums = [10, 20, 30]
+let nums = [10, 20, 30];
 let newMaps = nums.map((n) => n * 2);
 console.log(newMaps);
 
@@ -9,3 +9,15 @@ console.log(newNames);
 
 let newFilter = nums.filter((n) => n > 15);
 console.log(newFilter);
+
+let numbers = [1, 2, 3, 4, 5];
+let even = numbers.filter((n) => n % 2 === 0);
+console.log(even);
+
+
+let users = [
+  { name: "A", active: true },
+  { name: "B", active: false },
+];
+let activeUsers = users.filter((u) => u.active === true);
+console.log(activeUsers);
