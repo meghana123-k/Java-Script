a22c64fa89a49afbc5652cabcafecca77e17839f
meghana123-k <kothakondameghana3@gmail.com>
2026-02-15 14:53:43 +0530

    fix: correct script file reference in index.html and initialize variables in script.js

---
+++ TodoList/script.js
@@ -0,0 +1,12 @@
+tasks = []
+currentFilter = "all"
+searchQuery = ""
+
+const taskForm = document.querySelector('#task-form')
+const taskInput = document.querySelector('#task-input')
+const prioritySelect = document.querySelector('#priority-task')
+const taskList = document.querySelector('#task-list')
+const searchInput = document.querySelector('#search-input')
+const filterButtons = document.querySelector('#filter-buttons')
+const summarycounters = document.querySelector("#summary")
+
