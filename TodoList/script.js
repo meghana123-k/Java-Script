3c515e0ce246dc3f01994c6ecc0f316de4cad7f7
meghana123-k <kothakondameghana3@gmail.com>
2026-02-15 15:20:23 +0530

    refactor: update variable declarations and improve event listener setup in script.js

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,12 +1,70 @@
-tasks = []
-currentFilter = "all"
-searchQuery = ""
+tasks = [];
+currentFilter = "all";
+searchQuery = "";
 
-const taskForm = document.querySelector('#task-form')
-const taskInput = document.querySelector('#task-input')
-const prioritySelect = document.querySelector('#priority-task')
-const taskList = document.querySelector('#task-list')
-const searchInput = document.querySelector('#search-input')
-const filterButtons = document.querySelector('#filter-buttons')
-const summarycounters = document.querySelector("#summary")
+const taskForm = document.querySelector("#task-form");
+const taskInput = document.querySelector("#task-input");
+const prioritySelect = document.querySelector("#priority-select");
+const taskList = document.querySelector("#task-list");
+const searchInput = document.querySelector("#search-input");
+const filterButtons = document.querySelector(".filter-buttons");
+const summarycounters = document.querySelector("#summary");
 
+taskForm.addEventListener("submit", addTask);
+filterButtons.addEventListener("click", changeFilter);
+// searchInput.addEventListener('search', updateSearchQuery);
+// taskList.addEventListener('click', handleComplete)
+let counter = 0;
+function addTask(e) {
+  e.preventDefault();
+  const value = taskInput.value.trim();
+  if (!value) {
+    alert("Task must not be empty");
+    return;
+  }
+  const task = {};
+  task.id = counter;
+  task.title = value;
+  task.priority = prioritySelect.value;
+  task.completed = false;
+  task.createdAt = Date.now();
+  tasks.push(task);
+  console.log(tasks);
+
+  taskInput.value = "";
+  render();
+}
+function render() {
+  taskList.innerHTML = "";
+  let filtered = changeFilter();
+//   let serachQuery = updateSearchQuery();
+}
+function changeFilter(e) {
+  e.preventDefault();
+  let target = e.target.value.toLowerCase();
+  if (target === "completed") {
+    Arrays.from(tasks).forEach((task) => {
+      if (task.completed) {
+        taskList.style.display = "flex";
+      } else {
+        taskList.style.display = "none";
+      }
+    });
+  } else if (target === "pending") {
+    tasks.forEach((task) => {
+      if (task.completed) {
+        taskList.style.display = "none";
+    } else {
+        taskList.style.display = "flex";
+    }
+});
+} else if (target === "high priority") {
+    tasks.forEach((task) => {
+        if(task.priority === "high") {
+            taskList.style.display = "flex";
+        } else {
+            taskList.style.display = "none";
+        }
+    });
+  }
+}
